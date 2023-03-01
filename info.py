9ae8a9ba21d5a7240f330b94715cec8c4ec339b0
fah <73680207+ChutimaNakaew@users.noreply.github.com>
2023-03-01 15:40:51 +0700

    Update info.py

--- info.py
+++ info.py
@@ -4,10 +4,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument("txt")
 args = parser.parse_args()
 
-if(args.txt == 'name'){
+if args.txt == 'name':
   return 'Alice'
 }
-else if(args.txt == 'age'){
+else if args.txt == 'age':
   return 20
 }
    
